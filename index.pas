17fef70f326eeb46d8acc9389f6b2c81b6c6c97a
syifa25 <umrimarsyifa2@gmail.com>
2024-09-13 00:45:10 +0700

    buat code

---
+++ index.pas
@@ -0,0 +1,13 @@
+
+var
+nama : string;
+nim : longint;
+
+begin
+    write('Masukkan nama : ');
+    readln(nama);
+    write('Masukkan nim : ');
+    readln(nim);
+
+    write('Halo nama saya ', nama, ' dengan nim ', nim);
+end.
