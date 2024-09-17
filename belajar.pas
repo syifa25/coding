9c02da575ff0659b5170c28ec7c0538df664bfdf
syifa25 <umrimarsyifa2@gmail.com>
2024-09-17 17:21:03 +0700

    sama kayak yang sebelumnya

--- belajar.pas
+++ belajar.pas
@@ -3,7 +3,7 @@ uses crt;
 
 var
 nama : string;
-nilai_aktif, nilai_tugas, nilai_ujian, nilai_m_keaktifan, nilai_m_tugas, nilai_m_ujian, nilai_akhir : real;
+nilai_aktif, nilai_tugas, nilai_ujian, nilai_murni_keaktifan, nilai_murni_tugas, nilai_murni_ujian, nilai_akhir : real;
 
 begin
 clrscr;
@@ -16,10 +16,10 @@ clrscr;
     write ('Nilai Ujian      : ');
     readln(nilai_ujian);
 
-    nilai_m_keaktifan := nilai_aktif * 20/100;
-    nilai_m_tugas := nilai_tugas * 35/100;
-    nilai_m_ujian  := nilai_ujian * 45/100;
-    nilai_akhir := nilai_m_keaktifan + nilai_m_tugas + nilai_m_ujian;
+    nilai_murni_keaktifan := nilai_aktif * 20/100;
+    nilai_murni_tugas := nilai_tugas * 35/100;
+    nilai_murni_ujian  := nilai_ujian * 45/100;
+    nilai_akhir := nilai_murni_keaktifan + nilai_murni_tugas + nilai_murni_ujian;
 
     writeln ('----Nilai Akhir Mahasiswa----');
     write('Nilai Akhir : ', nilai_akhir:0:2);
